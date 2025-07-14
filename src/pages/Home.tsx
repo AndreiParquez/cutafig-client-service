564caf95f16886c7e455cd4b88d50afbbd8479af
AndreiParquez <jackparquez1@gmail.com>
2025-07-14 21:53:52 +0800

    refactor: clean up formatting and improve readability in Home component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -14,9 +14,9 @@ const Home: React.FC = () => {
   };
 
   const itemVariants = {
-    hidden: { 
-      opacity: 0, 
-      y: 30 
+    hidden: {
+      opacity: 0,
+      y: 30
     },
     visible: {
       opacity: 1,
@@ -28,9 +28,9 @@ const Home: React.FC = () => {
   };
 
   const sidebarItemVariants = {
-    hidden: { 
-      opacity: 0, 
-      x: 30 
+    hidden: {
+      opacity: 0,
+      x: 30
     },
     visible: {
       opacity: 1,
@@ -45,7 +45,7 @@ const Home: React.FC = () => {
       <h1>
         Featured Articles
       </h1>
-      <motion.section 
+      <motion.section
         className=" mx-auto px-4 py-8 grid md:grid-cols-3 gap-6"
         variants={containerVariants}
         initial="hidden"
@@ -55,7 +55,7 @@ const Home: React.FC = () => {
         {/* Left: Featured articles */}
         <div className="md:col-span-2 space-y-6">
           {/* Article 1 */}
-          <motion.div 
+          <motion.div
             className="flex flex-col md:flex-row bg-white overflow-hidden"
             variants={itemVariants}
           >
@@ -64,7 +64,7 @@ const Home: React.FC = () => {
               <div>
                 <span className="font-semibold text-lg hover:underline hover:!cursor-pointer ">Article 1</span>
                 <p className="text-sm text-gray-600 mt-2">
-                                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam minima ducimus provident earum culpa nobis officiis iste aperiam incidunt similique maxime neque laboriosam distinctio quia pariatur, obcaecati eius. Laudantium, quibusdam?
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit. Numquam minima ducimus provident earum culpa nobis officiis iste aperiam incidunt similique maxime neque laboriosam distinctio quia pariatur, obcaecati eius. Laudantium, quibusdam?
                   Hic placeat reiciendis inventore eveniet optio quas eius officiis error distinctio sint, iure autem odit pariatur corporis et illo dolor ullam excepturi? Ipsam voluptates minus reiciendis mollitia, delectus saepe. Tenetur?
                 </p>
               </div>
@@ -76,11 +76,11 @@ const Home: React.FC = () => {
           </motion.div>
 
           {/* Article 2 */}
-          <motion.div 
+          <motion.div
             className="flex flex-col md:flex-row-reverse bg-white  overflow-hidden"
             variants={itemVariants}
           >
-            <img src='../assets/images/image.png'  alt="Article 2" className="rounded-xl md:w-1/2 object-cover" />
+            <img src='../assets/images/image.png' alt="Article 2" className="rounded-xl md:w-1/2 object-cover" />
             <div className="p-4 flex flex-col justify-between">
               <div>
                 <h2 className="font-semibold text-lg">Article 2</h2>
@@ -100,12 +100,12 @@ const Home: React.FC = () => {
         {/* Right: Sidebar smaller articles */}
         <div className="space-y-4">
           {[1, 2, 3].map(i => (
-            <motion.div 
-              key={i} 
+            <motion.div
+              key={i}
               className="flex flex-col  bg-white overflow-hidden"
               variants={sidebarItemVariants}
             >
-              <img src='../assets/images/image.png'  alt="Small article" className="w-full rounded-xl h-44 object-cover" />
+              <img src='../assets/images/image.png' alt="Small article" className="w-full rounded-xl h-44 object-cover" />
               <div className="p-2 ">
                 <p className="text-xs text-gray-700">Lorem ipsum</p>
                 <p className="text-xs text-gray-500">Lorem ipsum dolor sit amet...</p>
@@ -115,16 +115,29 @@ const Home: React.FC = () => {
         </div>
 
         {/* View more button: span all columns on mobile */}
-        <div className="md:col-span-3 flex justify-center mt-6">
-          <motion.button 
+        <motion.div
+          className="md:col-span-3  flex justify-center  mt-6"
+          whileHover="hover"
+          initial="rest"
+          animate="rest"
+        >
+          <button
             className="flex items-center text-gray-700 hover:text-black text-sm"
-            whileHover={{ x: 8 }}
-            transition={{ type: "spring", stiffness: 300, damping: 20 }}
+            type="button"
           >
             View More
-            <motion.img src="../assets/icons/arrow.svg" alt="" className='ml-1 h-4 w-4' />
-          </motion.button>
-        </div>
+            <motion.img
+              variants={{
+                rest: { x: 0 },
+                hover: { x: 5 },
+              }}
+              transition={{ type: "spring", stiffness: 300 }}
+              src="../assets/icons/arrow.svg"
+              alt=""
+              className='ml-1 h-4 w-4'
+            />
+          </button>
+        </motion.div>
       </motion.section>
     </div>
   );
